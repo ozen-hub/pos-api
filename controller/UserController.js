ee95cd7b3e61b4b08423ff8a2fcbf6e05714e88a
ozen-hub <info.ozenhub@gmail.com>
2023-02-07 09:43:21 +0530

    2

--- controller/UserController.js
+++ controller/UserController.js
@@ -1,21 +1,24 @@
 const UserSchema= require('../model/User');
-// origin==>
+const bcrypt=require('bcrypt');
 const register =(request,response)=>{
-    const dto = new UserSchema({
-       name:request.body.name,
-       email:request.body.email,
-       password:request.body.password
+
+    bcrypt.hash(request.body.password, 10, function(err, hash) {
+        const dto = new UserSchema({
+            name:request.body.name,
+            email:request.body.email,
+            password:hash
+        });
+        dto.save().then(result=>{
+            let responseUserData={
+                userEmail:result.email,
+                token:'****',
+                status:201,
+                message:'success!'
+            }
+            response.status(201).json(responseUserData);
+        }).catch(error=>{
+            response.status(500).json(error);
+        })
     });
-    dto.save().then(result=>{
-        let responseUserData={
-            userEmail:result.email,
-            token:'****',
-            status:201,
-            message:'success!'
-        }
-        response.status(201).json(responseUserData);
-    }).catch(error=>{
-        response.status(500).json(error);
-    })
 }
 module.exports={register}
